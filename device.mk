e5ff2819d24a35a99adf7efc0333255200e07c11
Kai-Chieh Ku <kjackie@gmail.com>
2018-03-19 12:44:27 +0800

    Add prebuilt OOS camera binaries
    Change-Id: Ic8a1e48c78bfae35a001bb42379d915d23f51d2b

--- device.mk
+++ device.mk
@@ -40,5 +40,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt \
     $(LOCAL_PATH)/audio/mixer_paths_tasha.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tasha.xml
 
+# OnePlusCamera
+PRODUCT_PACKAGES += \
+    OnePlusCamera \
+    OnePlusCameraService \
+    OnePlusGallery
+
 # Inherit from oneplus msm8998-common
 $(call inherit-product, device/oneplus/msm8998-common/common.mk)
